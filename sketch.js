8f4f6a259424c26a3b8f1ff5b3669a4cf72aafbb
playmath <105438305+playmath@users.noreply.github.com>
2022-05-12 19:30:53 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,41 @@
+var logo;
+var singleplayer;
+var multiplayer;
+
+
+function preload(){
+logo = loadImage("Logo Transperant.png");
+    
+}
+
+function setup() {
+    createCanvas(windowWidth,windowHeight);
+
+    singleplayer = createButton("Single Player");
+    singleplayer.position(width/2 - 100,220);
+    singleplayer.class("button");
+    singleplayer.mouseClicked(singleplayeropen);
+
+    multiplayer = createButton("Multi Player");
+    multiplayer.position(width/2 - 100, 320);
+    multiplayer.class("button");
+    multiplayer.mouseClicked(multiplayeropen);
+
+
+}
+
+function draw() {
+    background("white");
+    imageMode(CENTER);
+  image(logo,width/2, 100,150,150);
+    drawSprites();
+
+}
+
+function singleplayeropen(){
+window.open("https://playmath.github.com/singleplayer");
+}
+
+function multiplayeropen(){
+window.open("https://playmath.github.com/multiplayer");
+}
